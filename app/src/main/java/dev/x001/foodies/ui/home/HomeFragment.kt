c6f5b7d55cdee93fe46ba8884330314c7e9faa22
John Patrick <john@x001.dev>
2023-03-07 21:28:54 +0800

    Add floating action buttons animations.

--- app/src/main/java/dev/x001/foodies/ui/home/HomeFragment.kt
+++ app/src/main/java/dev/x001/foodies/ui/home/HomeFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import dev.x001.foodies.R
 import dev.x001.foodies.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -33,6 +37,61 @@ class HomeFragment : Fragment() {
             textView.text = it
         }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.expandableFloatingActionButton.setOnClickListener {
+            onExpandableFABClicked()
+        }
+
+        binding.addRecipeFloatingActionButton.setOnClickListener {
+            Toast.makeText(requireContext(), "Add Recipe", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.filterFloatingActionButton.setOnClickListener {
+            Toast.makeText(requireContext(), "Filter", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private var fabClicked = false
+    private fun onExpandableFABClicked() {
+        setFABVisibility(fabClicked)
+        setFABAnimation(fabClicked)
+
+        fabClicked = !fabClicked
+    }
+
+    private fun setFABAnimation(fabClicked: Boolean) {
+        val rotateOpen: Animation = AnimationUtils.loadAnimation(requireContext(), R.anim.rotate_open_anim)
+        val rotateClose: Animation = AnimationUtils.loadAnimation(requireContext(), R.anim.rotate_close_anim)
+        val fromBottom: Animation = AnimationUtils.loadAnimation(requireContext(), R.anim.from_bottom_anim)
+        val toBottom: Animation = AnimationUtils.loadAnimation(requireContext(), R.anim.to_bottom_anim)
+
+        if (!fabClicked){
+            binding.addRecipeFloatingActionButton.startAnimation(fromBottom)
+            binding.filterFloatingActionButton.startAnimation(fromBottom)
+            binding.expandableFloatingActionButton.startAnimation(rotateOpen)
+        }else{
+            binding.addRecipeFloatingActionButton.startAnimation(toBottom)
+            binding.filterFloatingActionButton.startAnimation(toBottom)
+            binding.expandableFloatingActionButton.startAnimation(rotateClose)
+        }
+    }
+
+    private fun setFABVisibility(fabClicked: Boolean) {
+        if (!fabClicked){
+            binding.addRecipeFloatingActionButton.visibility = View.VISIBLE
+            binding.filterFloatingActionButton.visibility = View.VISIBLE
+            binding.addRecipeFloatingActionButton.isClickable = true
+            binding.filterFloatingActionButton.isClickable = true
+        }else{
+            binding.addRecipeFloatingActionButton.visibility = View.GONE
+            binding.filterFloatingActionButton.visibility = View.GONE
+            binding.addRecipeFloatingActionButton.isClickable = false
+            binding.filterFloatingActionButton.isClickable = false
+        }
     }
 
     override fun onDestroyView() {
