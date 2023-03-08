fa2e392c50af052d9ac7e36a390c2560c9352175
John Patrick <john@x001.dev>
2023-03-08 19:54:28 +0800

    Add activity_add_update_dish.xml layout.

--- app/src/main/java/dev/x001/foodies/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/dev/x001/foodies/view/activities/AddUpdateDishActivity.kt
@@ -2,14 +2,37 @@ package dev.x001.foodies.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.blogspot.atifsoftwares.animatoolib.Animatoo
 import dev.x001.foodies.R
+import dev.x001.foodies.databinding.ActivityAddUpdateDishBinding
 
-class AddUpdateDishActivity : AppCompatActivity() {
+class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityAddUpdateDishBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+        binding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.toolbar.setNavigationOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        binding.addImageImageView.setOnClickListener(this)
 
         Animatoo.animateSlideLeft(this)
+    }
+
+    override fun onClick(view: View?) {
+        if (view != null){
+            when(view.id){
+                R.id.add_image_imageView -> {
+                    Toast.makeText(this, "Add Image", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
