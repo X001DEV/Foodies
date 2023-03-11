42f4e9433c2413cb68dbf26a9bc4241285e53b02
John Patrick <john@x001.dev>
2023-03-11 15:27:34 +0800

    Add Constants.kt

---
+++ app/src/main/java/dev/x001/foodies/utils/Constants.kt
@@ -0,0 +1,36 @@
+package dev.x001.foodies.utils
+
+object Constants {
+
+    const val DISH_TYPE: String = "DishType"
+    const val DISH_CATEGORY: String = "DishCategory"
+    const val DISH_COOKING_TIME: String = "DishCookingTime"
+
+    val dishTypes = arrayListOf(
+        "Breakfast",
+        "Lunch",
+        "Snacks",
+        "Dinner",
+        "Salad",
+        "Side Dish",
+        "Dessert",
+        "Other"
+    )
+
+    val dishCategory = arrayListOf(
+        "Pizza",
+        "BBQ",
+        "Bakery",
+        "Burger",
+        "Cafe",
+        "Chicken",
+        "Dessert",
+        "Drinks",
+        "Hot Dogs",
+        "Juices",
+        "Sandwich",
+        "Tea & Coffee",
+        "Wraps",
+        "Other"
+    )
+}
