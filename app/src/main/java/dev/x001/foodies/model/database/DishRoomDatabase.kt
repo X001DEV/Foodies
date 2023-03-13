ab45321bce7a9f42e73332a6967708119ee996d6
John Patrick <john@x001.dev>
2023-03-13 16:38:01 +0800

    Add DishRoomDatabase.kt for database.

---
+++ app/src/main/java/dev/x001/foodies/model/database/DishRoomDatabase.kt
@@ -0,0 +1,32 @@
+package dev.x001.foodies.model.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import dev.x001.foodies.model.entities.Dish
+
+@Database(entities = [Dish::class], version = 1)
+abstract class DishRoomDatabase: RoomDatabase() {
+    companion object{
+        @Volatile
+        private var INSTANCE: DishRoomDatabase? = null
+
+        fun getDatabase(context: Context): DishRoomDatabase{
+            //if INSTANCE is not null, then return it,
+            //if it is, then create the database
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    DishRoomDatabase::class.java,
+                    "dish_ database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                //return instance
+                instance
+            }
+        }
+    }
+}
