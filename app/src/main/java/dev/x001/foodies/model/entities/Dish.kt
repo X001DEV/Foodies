2e33360836f142ad38f278fbf3d3c1f115c1f4c5
John Patrick <john@x001.dev>
2023-03-13 13:18:28 +0800

    Add Dish.kt entity.

--- app/src/main/java/dev/x001/foodies/model/entities/Dish.kt
+++ app/src/main/java/dev/x001/foodies/model/entities/Dish.kt
@@ -1,13 +1,23 @@
 package dev.x001.foodies.model.entities
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "dishes-table")
 data class Dish(
     val image: String,
     val imageSource: String,
-    val title: String,
+    val dish: String,
     val type: String,
     val category: String,
-    val ingredients: String
+    val ingredients: String,
+    @ColumnInfo(name ="cooking_time")
+    val cookingTime: String,
+    @ColumnInfo(name = "instructions")
+    val directionToCook: String,
+    @ColumnInfo(name = "favorite_dish")
+    var favoriteDish: Boolean = false,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0
 )
