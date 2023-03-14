8c9f095af0e9e1492e2a9d675a0a745298ec84d6
John Patrick <john@x001.dev>
2023-03-14 13:24:08 +0800

    Refactor app.

--- app/src/main/java/dev/x001/foodies/view/fragments/RandomDishFragment.kt
+++ app/src/main/java/dev/x001/foodies/view/fragments/RandomDishFragment.kt
@@ -7,12 +7,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import dev.x001.foodies.databinding.FragmentDashboardBinding
-import dev.x001.foodies.viewmodel.DashboardViewModel
+import dev.x001.foodies.databinding.FragmentRandomDishBinding
+import dev.x001.foodies.viewmodel.NotificationsViewModel
 
-class DashboardFragment : Fragment() {
+class RandomDishFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentRandomDishBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,14 +23,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val notificationsViewModel =
+            ViewModelProvider(this).get(NotificationsViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentRandomDishBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textNotifications
+        notificationsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
