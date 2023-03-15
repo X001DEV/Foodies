5a0a49156c4564af8c44a6fa7f41042cd90843e8
John Patrick <john@x001.dev>
2023-03-15 20:18:48 +0800

    Update Dish.kt to parcelable.

--- app/src/main/java/dev/x001/foodies/model/entities/Dish.kt
+++ app/src/main/java/dev/x001/foodies/model/entities/Dish.kt
@@ -1,5 +1,7 @@
 package dev.x001.foodies.model.entities
 
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
@@ -20,4 +22,45 @@ data class Dish(
     var favoriteDish: Boolean = false,
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0
-)
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readByte() != 0.toByte(),
+        parcel.readInt()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(image)
+        parcel.writeString(imageSource)
+        parcel.writeString(dish)
+        parcel.writeString(type)
+        parcel.writeString(category)
+        parcel.writeString(ingredients)
+        parcel.writeString(cookingTime)
+        parcel.writeString(directionToCook)
+        parcel.writeByte(if (favoriteDish) 1 else 0)
+        parcel.writeInt(id)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Dish> {
+        override fun createFromParcel(parcel: Parcel): Dish {
+            return Dish(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Dish?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
