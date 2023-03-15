fc50fdef59c9ba52777bb7861a7fef3f1a44af04
John Patrick <john@x001.dev>
2023-03-15 22:02:36 +0800

    Display data to fragment_dish_details.xml

--- app/src/main/java/dev/x001/foodies/view/fragments/DishDetailsFragment.kt
+++ app/src/main/java/dev/x001/foodies/view/fragments/DishDetailsFragment.kt
@@ -7,11 +7,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import dev.x001.foodies.R
+import dev.x001.foodies.databinding.FragmentAllDishesBinding
+import dev.x001.foodies.databinding.FragmentDishDetailsBinding
 
 class DishDetailsFragment : Fragment() {
 
     private val args: DishDetailsFragmentArgs by navArgs()
+    private lateinit var binding: FragmentDishDetailsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +28,22 @@ class DishDetailsFragment : Fragment() {
 
         Log.i("DISH", args.dishDetails.dish)
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dish_details, container, false)
+        binding = FragmentDishDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.dishTextView.text = args.dishDetails.dish
+        binding.categoryAndTypeTextView.text = "${args.dishDetails.category} · ${args.dishDetails.type}"
+        binding.ingredientsTextView.text = args.dishDetails.ingredients
+        binding.directionsTextView.text = args.dishDetails.directionToCook
+        binding.cookingTimeTextView.text = "${args.dishDetails.cookingTime} minutes"
+
+        Glide.with(requireContext())
+            .load(args.dishDetails.image)
+            .into(binding.dishImageView)
     }
 
 }
